a61d30bfe74f902002bcadbe5ffaa7f76b4ffc8f
Aeshachokshi <aeshachokshi693@gmail.com>
2022-05-17 20:22:04 -0400

    Aesha commit on 14th may 2022 @10:58AM

---
+++ src/test/java/com/Orangehrm/testCases/TC003_UserLoginTestCase.java
@@ -0,0 +1,34 @@
+package com.Orangehrm.testCases;
+
+
+
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+
+import com.Orangehrm.pageObjects.DashboardPage;
+import com.Orangehrm.pageObjects.LoginPage;
+public class TC003_UserLoginTestCase extends BaseClass {
+	
+	
+	@Test
+	public void loginTest() {
+		
+		LoginPage lp = new LoginPage(driver);
+		DashboardPage dp = new DashboardPage(driver);
+		lp.setUsername(prop.getProperty("appusername"));
+		lp.setPassword(prop.getProperty("apppassword"));
+		lp.clickLoginBtn();
+		
+	boolean status = dp.dashboardLinkDisplayedStatus();
+	Assert.assertEquals(status, true , "User did not logged successfully");
+	logger.info("User logged into application successfully");
+		
+	}
+	
+	
+
+	
+	
+
+}
